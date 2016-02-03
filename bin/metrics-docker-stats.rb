425dfa9fcc76f2607897579c900ed27fa9aedf65
Tatyana Arenburg <tata2000@users.noreply.github.com>
2016-02-03 14:42:59 +0200

    Update metric-docker-stats.rb
    Allowing to get full response from docker for /containers/json

--- bin/metrics-docker-stats.rb
+++ bin/metrics-docker-stats.rb
@@ -103,37 +103,53 @@ class DockerStatsMetrics < Sensu::Plugin::Metric::CLI::Graphite
     end
   end
 
-  def docker_api(path)
-    if config[:docker_protocol] == 'unix'
-      begin
-        NetX::HTTPUnix.start("unix://#{config[:docker_host]}") do |http|
-          request = Net::HTTP::Get.new "/#{path}"
-          http.request request do |response|
-            response.read_body do |chunk|
-              @response = JSON.parse(chunk)
-              http.finish
-            end
-          end
+  def docker_api(path, full_body=false)
+    begin
+      if config[:docker_protocol] == 'unix'
+        request = Net::HTTP::Get.new "/#{path}"
+        NetX::HTTPUnix.start("unix://#{config[:docker_host]}") do | http |
+          get_response(full_body, http, request)
         end
-      rescue NoMethodError
-        # using http.finish to prematurely kill the stream causes this exception.
-        return @response
+
+      else
+        uri = URI("#{config[:docker_protocol]}://#{config[:docker_host]}/#{path}")
+
+
+        request = Net::HTTP::Get.new uri.request_uri
+        Net::HTTP.start(uri.host, uri.port) do | http |
+          get_response(full_body, http, request)
+        end
       end
-    else
-      uri = URI("#{config[:docker_protocol]}://#{config[:docker_host]}/#{path}")
-      begin
-        Net::HTTP.start(uri.host, uri.port) do |http|
-          request = Net::HTTP::Get.new uri.request_uri
-          http.request request do |response|
-            response.read_body do |chunk|
-              @response = JSON.parse(chunk)
-              http.finish
-            end
-          end
-        end
-      rescue NoMethodError
+
+
+    rescue NoMethodError
         # using http.finish to prematurely kill the stream causes this exception.
         return @response
+
+    end
+  end
+
+  def get_response(full_body, http, request)
+    if full_body
+      get_full_response(http, request)
+    else
+      get_single_chunk(http, request)
+    end
+  end
+
+  def get_full_response(http, request)
+    http.request request do |response|
+      @response = JSON.parse(response.read_body)
+    end
+    http.finish
+    @response
+  end
+
+  def get_single_chunk(http, request)
+    http.request request do |response|
+      response.read_body do |chunk|
+        @response = JSON.parse(chunk)
+        http.finish
       end
     end
   end
@@ -141,7 +157,7 @@ class DockerStatsMetrics < Sensu::Plugin::Metric::CLI::Graphite
   def list_containers
     list = []
     path = 'containers/json'
-    @containers = docker_api(path)
+    @containers = docker_api(path, true)
 
     @containers.each do |container|
       if config[:friendly_names]
