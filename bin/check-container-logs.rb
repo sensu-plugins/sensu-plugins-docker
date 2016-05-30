3630bbb3cfac863552bc0bfb823f68dcd4969ec1
aksentyev <grigory.aksentiev@gmail.com>
2016-05-30 13:51:11 +0300

    add seconds_ago option

--- bin/check-container-logs.rb
+++ bin/check-container-logs.rb
@@ -76,18 +76,27 @@ insensitive',
          short: '-t HOURS',
          long: '--hours-ago HOURS',
          required: false
+         
+  option :seconds_ago,
+       description: 'Amount of time in seconds to look back for log strings',
+       short: '-s SECONDS',
+       long: '--seconds-ago SECONDS',
+       required: false
 
-  def calculate_timestamp(hours)
-    seconds_ago = hours.to_i * 3600
+  def calculate_timestamp(seconds_ago=nil)
+    seconds_ago = yield if block_given?
     (Time.now - seconds_ago).to_i
   end
 
   def process_docker_logs(container_name)
     client = create_docker_client
     path = "/containers/#{container_name}/logs?stdout=true&stderr=true"
-    if config.key? :hours_ago
-      path = "#{path}&since=#{calculate_timestamp config[:hours_ago]}"
+    if config.key? :hours_ago 
+      timestamp = calculate_timestamp { config[:hours_ago].to_i * 3600 }
+    elsif config.key? :seconds_ago
+      timestamp = calculate_timestamp config[:seconds_ago].to_i
     end
+    path = "#{path}&since=#{timestamp}"
     req = Net::HTTP::Get.new path
 
     client.request req do |response|
